5e301b619fb9c57d42290d2571c76e212a082c76
Bogomil Stoev <bogomil.stoev01@gmail.com>
2024-07-06 17:46:39 +0300

    moved bean config to separate package

--- src/main/java/com/tinqinacademy/hotel/services/configurations/ApplicationBeanConfiguration.java
+++ src/main/java/com/tinqinacademy/hotel/services/configurations/ApplicationBeanConfiguration.java
@@ -1,4 +1,4 @@
-package com.tinqinacademy.hotel.services;
+package com.tinqinacademy.hotel.services.configurations;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
