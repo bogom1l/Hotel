7d664365f01dd1456feb60efdb1ac2646b59d493
bogom1l <bogomil.stoev01@gmail.com>
2024-07-17 16:51:10 +0300

    Added DDL DB Query
    and connected to db on homepc

--- api/src/main/java/com/tinqinacademy/hotel/api/enums/BedSize.java
+++ api/src/main/java/com/tinqinacademy/hotel/api/enums/BedSize.java
@@ -3,8 +3,15 @@ package com.tinqinacademy.hotel.api.enums;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
+// TODO: rename to Bed , add a field 'capacity'
+
 public enum BedSize {
-    SINGLE("single"), SMALL_DOUBLE("smallDouble"), DOUBLE("double"), KING_SIZE("kingSize"), QUEEN_SIZE("queensSize"), UNKNOWN("");
+    SINGLE("single"),
+    SMALL_DOUBLE("smallDouble"),
+    DOUBLE("double"),
+    KING_SIZE("kingSize"),
+    QUEEN_SIZE("queensSize"),
+    UNKNOWN("");
 
     private final String code;
 
