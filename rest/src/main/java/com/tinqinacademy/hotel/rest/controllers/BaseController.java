ed1b6d430b4911fd26864e554daf6771ff04324c
Bogomil Stoev <bogomil.stoev01@gmail.com>
2024-07-31 12:27:59 +0300

    cleaned up

--- rest/src/main/java/com/tinqinacademy/hotel/rest/controllers/BaseController.java
+++ rest/src/main/java/com/tinqinacademy/hotel/rest/controllers/BaseController.java
@@ -9,47 +9,22 @@ import org.springframework.http.ResponseEntity;
 public abstract class BaseController {
 
     protected ResponseEntity<?> handle(Either<ErrorsWrapper, ? extends OperationOutput> output) {
-        if(output.isLeft()){
+        if (output.isLeft()) {
             return error(output);
         }
         return new ResponseEntity<>(output.get(), HttpStatus.OK);
     }
 
     protected ResponseEntity<?> handleWithStatus(Either<ErrorsWrapper, ? extends OperationOutput> output, HttpStatus status) {
-        if(output.isLeft()){
+        if (output.isLeft()) {
             return error(output);
         }
         return new ResponseEntity<>(output.get(), status);
     }
 
-    private ResponseEntity<?> error(Either<ErrorsWrapper, ? extends OperationOutput> output){
+    private ResponseEntity<?> error(Either<ErrorsWrapper, ? extends OperationOutput> output) {
         ErrorsWrapper errorWrapper = output.getLeft();
         return new ResponseEntity<>(errorWrapper.getErrors(), errorWrapper.getHttpStatus());
     }
 
-    /*
-
-        protected<O extends OperationOutput> ResponseEntity<?> handleWithCode(Either<ErrorWrapper,O> result, HttpStatus status){
-        if (result.isRight()) {
-            O output = result.get();
-            return new ResponseEntity<>(output, status);
-        } else {
-            return error(result);
-        }
-    }
-
-    protected<O extends OperationOutput> ResponseEntity<?> handle(Either<ErrorWrapper,O> result){
-        if (result.isRight()) {
-            O output = result.get();
-            return new ResponseEntity<>(output, HttpStatus.OK);
-        } else {
-            return error(result);
-        }
-    }
-
-    private<O extends OperationOutput> ResponseEntity<?> error(Either<ErrorWrapper,O> result){
-        ErrorWrapper errorWrapper = result.getLeft();
-        return new ResponseEntity<>(errorWrapper.getErrors(), errorWrapper.getErrorCode());
-    }
-     */
 }
