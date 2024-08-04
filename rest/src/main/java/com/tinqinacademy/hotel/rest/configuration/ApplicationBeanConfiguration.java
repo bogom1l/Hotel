8ff54e6c340e45efd4c49f001e52ddc800079b4b
bogom1l <bogomil.stoev01@gmail.com>
2024-08-04 18:42:49 +0300

    Added comments to the ObjectMapper

--- rest/src/main/java/com/tinqinacademy/hotel/rest/configuration/ApplicationBeanConfiguration.java
+++ rest/src/main/java/com/tinqinacademy/hotel/rest/configuration/ApplicationBeanConfiguration.java
@@ -13,10 +13,10 @@ public class ApplicationBeanConfiguration {
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JavaTimeModule());
-        mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
-        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        mapper.registerModule(new JavaTimeModule()); // Java 8 date/time type `java.time.LocalDate` not supported by default
+        mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS); // Jackson will handle empty beans by serializing them to an empty JSON object ( {} ) instead of throwing an exception
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS); // better readability of dates
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL); // exclude null fields from JSON
         return mapper;
     }
 }
