31dea1fc4f97c16b4a518b9463b1dae9eb07c40a
bogom1l <bogomil.stoev01@gmail.com>
2024-08-16 11:55:55 +0300

    Update RoomsToCheckAvailableRoomOutput.java

--- core/src/main/java/com/tinqinacademy/hotel/core/converters/room/RoomsToCheckAvailableRoomOutput.java
+++ core/src/main/java/com/tinqinacademy/hotel/core/converters/room/RoomsToCheckAvailableRoomOutput.java
@@ -1,30 +1,30 @@
-//package com.tinqinacademy.hotel.core.converters.room;
-//
-//import com.tinqinacademy.hotel.api.operations.hotel.checkavailableroom.CheckAvailableRoomOutput;
-//import com.tinqinacademy.hotel.persistence.model.Room;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.core.convert.converter.Converter;
-//import org.springframework.stereotype.Component;
-//
-//import java.util.List;
-//import java.util.UUID;
-//
-//@Component
-//@Slf4j
-//public class RoomsToCheckAvailableRoomOutput implements Converter<List<Room>, CheckAvailableRoomOutput> {
-//    @Override
-//    public CheckAvailableRoomOutput convert(List<Room> source) {
-//        log.info("Started Converter - Room to CheckAvailableRoomOutput");
-//
-//        List<String> ids = source.stream().map(Room::getId).map(UUID::toString).toList();
-//
-//        CheckAvailableRoomOutput checkAvailableRoomOutput =
-//                CheckAvailableRoomOutput
-//                        .builder()
-//                        .ids(ids)
-//                        .build();
-//
-//        log.info("Ended Converter - Room to CheckAvailableRoomOutput");
-//        return checkAvailableRoomOutput;
-//    }
-//}
+package com.tinqinacademy.hotel.core.converters.room;
+
+import com.tinqinacademy.hotel.api.operations.hotel.checkavailableroom.CheckAvailableRoomOutput;
+import com.tinqinacademy.hotel.persistence.model.Room;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.UUID;
+
+@Component
+@Slf4j
+public class RoomsToCheckAvailableRoomOutput implements Converter<List<Room>, CheckAvailableRoomOutput> {
+    @Override
+    public CheckAvailableRoomOutput convert(List<Room> source) {
+        log.info("Started Converter - Room to CheckAvailableRoomOutput");
+
+        List<String> ids = source.stream().map(Room::getId).map(UUID::toString).toList();
+
+        CheckAvailableRoomOutput checkAvailableRoomOutput =
+                CheckAvailableRoomOutput
+                        .builder()
+                        .ids(ids)
+                        .build();
+
+        log.info("Ended Converter - Room to CheckAvailableRoomOutput");
+        return checkAvailableRoomOutput;
+    }
+}
