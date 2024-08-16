812bc9d24ff7aab31f2355812b944521747f6ac7
bogom1l <bogomil.stoev01@gmail.com>
2024-08-16 21:15:29 +0300

    Implemented endpoint GetReport

--- core/src/main/java/com/tinqinacademy/hotel/core/converters/guest/GuestToGuestOutputBuilder.java
+++ core/src/main/java/com/tinqinacademy/hotel/core/converters/guest/GuestToGuestOutputBuilder.java
@@ -1,28 +1,28 @@
-//package com.tinqinacademy.hotel.core.converters.guest;
-//
-//import com.tinqinacademy.hotel.api.operations.system.getreport.GuestOutput;
-//import com.tinqinacademy.hotel.persistence.model.Guest;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.core.convert.converter.Converter;
-//import org.springframework.stereotype.Component;
-//
-//@Slf4j
-//@Component
-//public class GuestToGuestOutputBuilder implements Converter<Guest, GuestOutput.GuestOutputBuilder> {
-//    @Override
-//    public GuestOutput.GuestOutputBuilder convert(Guest source) {
-//        log.info("Started Converter - Guest to GuestOutput");
-//
-//        GuestOutput.GuestOutputBuilder guestOutput = GuestOutput.builder()
-//                .firstName(source.getFirstName())
-//                .lastName(source.getLastName())
-//                .phoneNo(source.getPhoneNumber())
-//                .idCardNo(source.getIdCardNumber())
-//                .idCardValidity(source.getIdCardValidity())
-//                .idCardIssueAuthority(source.getIdCardIssueAuthority())
-//                .idCardIssueDate(source.getIdCardIssueDate());
-//
-//        log.info("Ended Converter - Guest to GuestOutput");
-//        return guestOutput;
-//    }
-//}
+package com.tinqinacademy.hotel.core.converters.guest;
+
+import com.tinqinacademy.hotel.api.operations.system.getreport.GuestOutput;
+import com.tinqinacademy.hotel.persistence.model.Guest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class GuestToGuestOutputBuilder implements Converter<Guest, GuestOutput.GuestOutputBuilder> {
+    @Override
+    public GuestOutput.GuestOutputBuilder convert(Guest source) {
+        log.info("Started Converter - Guest to GuestOutput");
+
+        GuestOutput.GuestOutputBuilder guestOutput = GuestOutput.builder()
+                .firstName(source.getFirstName())
+                .lastName(source.getLastName())
+                .phoneNumber(source.getPhoneNumber())
+                .idCardNumber(source.getIdCardNumber())
+                .idCardValidity(source.getIdCardValidity())
+                .idCardIssueAuthority(source.getIdCardIssueAuthority())
+                .idCardIssueDate(source.getIdCardIssueDate());
+
+        log.info("Ended Converter - Guest to GuestOutput");
+        return guestOutput;
+    }
+}
--- rest/src/main/java/com/tinqinacademy/hotel/rest/controllers/SystemController.java
+++ rest/src/main/java/com/tinqinacademy/hotel/rest/controllers/SystemController.java
@@ -1,6 +1,7 @@
 package com.tinqinacademy.hotel.rest.controllers;
 
 
+import com.tinqinacademy.hotel.api.error.ErrorsWrapper;
 import com.tinqinacademy.hotel.api.operations.system.createroom.CreateRoomInput;
 import com.tinqinacademy.hotel.api.operations.system.createroom.CreateRoomOperation;
 import com.tinqinacademy.hotel.api.operations.system.deleteroom.DeleteRoomInput;
@@ -9,6 +10,7 @@ import com.tinqinacademy.hotel.api.operations.system.getallusers.GetAllUsersInpu
 import com.tinqinacademy.hotel.api.operations.system.getallusers.GetAllUsersOperation;
 import com.tinqinacademy.hotel.api.operations.system.getreport.GetReportInput;
 import com.tinqinacademy.hotel.api.operations.system.getreport.GetReportOperation;
+import com.tinqinacademy.hotel.api.operations.system.getreport.GetReportOutput;
 import com.tinqinacademy.hotel.api.operations.system.registerguest.RegisterGuestInput;
 import com.tinqinacademy.hotel.api.operations.system.registerguest.RegisterGuestOperation;
 import com.tinqinacademy.hotel.api.operations.system.updatepartiallyroom.UpdatePartiallyRoomInput;
@@ -19,6 +21,7 @@ import com.tinqinacademy.hotel.api.restroutes.RestApiRoutes;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.vavr.control.Either;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,7 +30,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 public class SystemController extends BaseController {
-//    private final GetReportOperation getReport;
+    private final GetReportOperation getReport;
 //    private final GetAllUsersOperation getAllUsersByPartialName;
     private final RegisterGuestOperation registerGuest;
 //    private final CreateRoomOperation createRoom;
@@ -45,38 +48,40 @@ public class SystemController extends BaseController {
     public ResponseEntity<?> registerGuest(@RequestBody RegisterGuestInput input) {
         return handleWithStatus(registerGuest.process(input), HttpStatus.CREATED);
     }
-//
-//    @Operation(summary = "Provides a report based on various criteria",
-//            description = "Provides a report based on various criteria")
-//    @ApiResponses(value = {
-//            @ApiResponse(responseCode = "200", description = "Report generated successfully"),
-//            @ApiResponse(responseCode = "400", description = "Invalid request parameters")})
-//    @GetMapping(RestApiRoutes.GET_REPORT)
-//    public ResponseEntity<?> getReport(@RequestParam(required = false) String startDate,
-//                                       @RequestParam(required = false) String endDate,
-//                                       @RequestParam(required = false) String firstName,
-//                                       @RequestParam(required = false) String lastName,
-//                                       @RequestParam(required = false) String phoneNumber,
-//                                       @RequestParam(required = false) String idCardNumber,
-//                                       @RequestParam(required = false) String idCardValidity,
-//                                       @RequestParam(required = false) String idCardIssueAuthority,
-//                                       @RequestParam(required = false) String idCardIssueDate,
-//                                       @RequestParam(required = false) String roomNumber) {
-//        GetReportInput input = GetReportInput.builder()
-//                .startDate(startDate)
-//                .endDate(endDate)
-//                .firstName(firstName)
-//                .lastName(lastName)
-//                .phoneNumber(phoneNumber)
-//                .idCardNumber(idCardNumber)
-//                .idCardValidity(idCardValidity)
-//                .idCardIssueAuthority(idCardIssueAuthority)
-//                .idCardIssueDate(idCardIssueDate)
-//                .roomNumber(roomNumber)
-//                .build();
-//
-//        return handle(getReport.process(input));
-//    }
+
+    @Operation(summary = "Provides a report based on various criteria",
+            description = "Provides a report based on various criteria")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Report generated successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid request parameters")})
+    @GetMapping(RestApiRoutes.GET_REPORT)
+    public ResponseEntity<?> getReport(@RequestParam(required = false) String startDate,
+                                       @RequestParam(required = false) String endDate,
+                                       @RequestParam(required = false) String firstName,
+                                       @RequestParam(required = false) String lastName,
+                                       @RequestParam(required = false) String phoneNumber,
+                                       @RequestParam(required = false) String idCardNumber,
+                                       @RequestParam(required = false) String idCardValidity,
+                                       @RequestParam(required = false) String idCardIssueAuthority,
+                                       @RequestParam(required = false) String idCardIssueDate,
+                                       @RequestParam(required = false) String roomNumber) {
+        GetReportInput input = GetReportInput.builder()
+                .startDate(startDate)
+                .endDate(endDate)
+                .firstName(firstName)
+                .lastName(lastName)
+                .phoneNumber(phoneNumber)
+                .idCardNumber(idCardNumber)
+                .idCardValidity(idCardValidity)
+                .idCardIssueAuthority(idCardIssueAuthority)
+                .idCardIssueDate(idCardIssueDate)
+                .roomNumber(roomNumber)
+                .build();
+
+        Either<ErrorsWrapper, GetReportOutput> result = getReport.process(input);
+
+        return handle(result);
+    }
 //
 //    @Operation(summary = "Create a room", description = "Create a room")
 //    @ApiResponses(value = {
