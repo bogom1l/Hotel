f5176086aff64daaa95c2c7d9de6e8b0b0b1ebc8
bogom1l <bogomil.stoev01@gmail.com>
2024-08-18 12:51:52 +0300

    Refactored ErrorHandler

--- core/src/main/java/com/tinqinacademy/hotel/core/errorhandler/ErrorHandler.java
+++ core/src/main/java/com/tinqinacademy/hotel/core/errorhandler/ErrorHandler.java
@@ -17,55 +17,57 @@ import static io.vavr.Predicates.instanceOf;
 
 @Component
 public class ErrorHandler {
-
     public ErrorsWrapper handleErrors(Throwable throwable) {
-        List<Error> errors = new ArrayList<>();
-
-        HttpStatus status = Match(throwable).of(
-                Case($(instanceOf(MethodArgumentNotValidException.class)), ex -> handleMethodArgumentNotValidException(ex, errors)),
-                Case($(instanceOf(ValidationException.class)), ex -> handleValidationException(ex, errors)),
-                Case($(instanceOf(RoomNotAvailableException.class)), ex -> handleRoomNotAvailableException(ex, errors)),
-                Case($(instanceOf(HotelException.class)), ex -> handleHotelException(ex, errors)),
-                Case($(), ex -> handleGenericException(ex, errors))
+        return Match(throwable).of(
+                Case($(instanceOf(MethodArgumentNotValidException.class)), ex -> handleMethodArgumentNotValidException(ex)),
+                Case($(instanceOf(ValidationException.class)), ex -> handleValidationException(ex)),
+                Case($(instanceOf(RoomNotAvailableException.class)), ex -> handleRoomNotAvailableException(ex)),
+                Case($(instanceOf(HotelException.class)), ex -> handleHotelException(ex)),
+                Case($(), ex -> handleGenericException(ex))
         );
+    }
 
+    private static Error createError(String field, String message) {
+        return Error.builder()
+                .field(field)
+                .message(message)
+                .build();
+    }
+
+    private static ErrorsWrapper createErrorsWrapper(List<Error> errors, HttpStatus status) {
         return ErrorsWrapper.builder()
                 .errors(errors)
                 .httpStatus(status)
                 .build();
     }
 
-    private HttpStatus handleValidationException(ValidationException ex, List<Error> errors) {
-        ex.getViolations().forEach(violation -> errors.add(Error.builder()
-                .field(violation.getField())
-                .message(violation.getMessage())
-                .build()));
-        return HttpStatus.BAD_REQUEST;
+    private static ErrorsWrapper handleValidationException(ValidationException ex) {
+        List<Error> errors = new ArrayList<>();
+        ex.getViolations().forEach(violation -> errors.add(createError(violation.getField(), violation.getMessage())));
 
+        return createErrorsWrapper(errors, HttpStatus.BAD_REQUEST);
     }
 
-    private HttpStatus handleMethodArgumentNotValidException(MethodArgumentNotValidException ex, List<Error> errors) {
+    private static ErrorsWrapper handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        List<Error> errors = new ArrayList<>();
         ex.getBindingResult().getFieldErrors()
-                .forEach(error ->
-                        errors.add(Error.builder()
-                                .field(error.getField())
-                                .message(error.getDefaultMessage())
-                                .build()));
-        return HttpStatus.BAD_REQUEST;
+                .forEach(error -> errors.add(createError(error.getField(), error.getDefaultMessage())));
+
+        return createErrorsWrapper(errors, HttpStatus.BAD_REQUEST);
     }
 
-    private HttpStatus handleRoomNotAvailableException(RoomNotAvailableException ex, List<Error> errors) {
-        errors.add(Error.builder().message(ex.getMessage()).build());
-        return HttpStatus.FORBIDDEN;
+    private static ErrorsWrapper handleRoomNotAvailableException(RoomNotAvailableException ex) {
+        List<Error> errors = List.of(createError(null, ex.getMessage()));
+        return createErrorsWrapper(errors, HttpStatus.FORBIDDEN);
     }
 
-    private HttpStatus handleHotelException(HotelException ex, List<Error> errors) {
-        errors.add(Error.builder().message(ex.getMessage()).build());
-        return HttpStatus.NOT_FOUND;
+    private static ErrorsWrapper handleHotelException(HotelException ex) {
+        List<Error> errors = List.of(createError(null, ex.getMessage()));
+        return createErrorsWrapper(errors, HttpStatus.NOT_FOUND);
     }
 
-    private HttpStatus handleGenericException(Throwable ex, List<Error> errors) {
-        errors.add(Error.builder().message(ex.getMessage()).build());
-        return HttpStatus.INTERNAL_SERVER_ERROR;
+    private static ErrorsWrapper handleGenericException(Throwable ex) {
+        List<Error> errors = List.of(createError(null, ex.getMessage()));
+        return createErrorsWrapper(errors, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
